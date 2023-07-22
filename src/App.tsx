975c238a7491d528e4bbcc6ca7fbd798a4c90735
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-07-23 02:13:11 +0900

    feat: router provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useState } from 'react';
+
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>app</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
