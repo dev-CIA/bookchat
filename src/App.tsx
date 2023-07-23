f67b00ffd228e4a69019ac50e57561e5549e956d
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-07-23 22:49:47 +0900

    feat: global style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react';
-
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <RouterProvider router={router} />
+      <MantineProvider
+        theme={{
+          globalStyles: () => ({
+            '*, *::before, *::after': {
+              boxSizing: 'border-box',
+            },
+          }),
+          primaryColor: 'teal',
+        }}
+        withCSSVariables
+        withGlobalStyles
+        withNormalizeCSS>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </>
   );
 }
