04f3e5d838f21e0164bd6628db8aa523cab65c78
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-07-25 23:25:24 +0900

    feat: links 상수화

---
+++ src/constants/links.ts
@@ -0,0 +1,22 @@
+const links = {
+  mainLinks: [
+    {
+      link: '/chat',
+      label: 'Book Chat',
+    },
+    {
+      link: '/recommend',
+      label: '추천받기',
+    },
+    {
+      link: '/mylibrary',
+      label: '내 서재',
+    },
+    {
+      link: '/profile',
+      label: '관리',
+    },
+  ],
+};
+
+export default links;
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,27 +3,7 @@ import { MainHeader, MobileHeader, MobileFooter } from '../components/root';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { AppShell, createStyles } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
-
-const mockLinks = {
-  mainLinks: [
-    {
-      link: '/chat',
-      label: 'Book Chat',
-    },
-    {
-      link: '/recommend',
-      label: '추천받기',
-    },
-    {
-      link: '/mylibrary',
-      label: '내 서재',
-    },
-    {
-      link: '/profile',
-      label: '관리',
-    },
-  ],
-};
+import { links } from '../constants';
 
 const useStyles = createStyles(() => ({
   wrapper: {
@@ -48,8 +28,8 @@ const Root = () => {
         w={'80%'}
         miw={350}
         className={classes.wrapper}
-        header={mediumScreen ? <MainHeader mainLinks={mockLinks.mainLinks} /> : <MobileHeader />}
-        footer={mediumScreen ? <></> : <MobileFooter mainLinks={mockLinks.mainLinks} />}>
+        header={mediumScreen ? <MainHeader mainLinks={links.mainLinks} /> : <MobileHeader />}
+        footer={mediumScreen ? <></> : <MobileFooter mainLinks={links.mainLinks} />}>
         <Outlet />
       </AppShell>
     </>
