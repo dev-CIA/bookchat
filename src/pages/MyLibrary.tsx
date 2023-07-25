5ef84d1335c8c295d9b0d99a3ac34ff4fb2dac41
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-07-25 13:35:04 +0900

    feat: MyLibrary UI 초기버전 구현

--- src/pages/MyLibrary.tsx
+++ src/pages/MyLibrary.tsx
@@ -1,8 +1,30 @@
+import { Books } from '../components/myLibrary';
+import { ActionIcon, TextInput, createStyles } from '@mantine/core';
+import { BsBook, BsSearch } from 'react-icons/bs';
 import React from 'react';
 
+const useStyles = createStyles(() => ({
+  form: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
+
 const MyLibrary = () => {
-  console.log();
-  return <>MyLibrary</>;
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <form className={classes.form}>
+        <TextInput placeholder="검색" icon={<BsBook />} w={'60%'} miw={300} />
+        <ActionIcon variant="filled" size={'lg'} title="search">
+          <BsSearch />
+        </ActionIcon>
+      </form>
+      <Books />
+    </>
+  );
 };
 
 export default MyLibrary;
