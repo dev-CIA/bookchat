8a8a9708ad19a7fed54218bb354e5693da11ad23
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-01 18:01:01 +0900

    feat: keydown 이벤트 발생시 메시지 전송되는 기능 구현

--- src/pages/BookChat.tsx
+++ src/pages/BookChat.tsx
@@ -73,123 +73,123 @@ interface Message {
   sender: string;
 }
 
-const mockData = [
-  {
-    id: 123,
-    content: '안녕',
-    sender: 'user',
-  },
-  {
-    id: 456,
-    content: '무슨 얘기를 할까요?',
-    sender: 'assistant',
-  },
-  {
-    id: 234,
-    content: '안녕',
-    sender: 'user',
-  },
-  {
-    id: 1233,
-    content: '무슨 얘기를 할까요?',
-    sender: 'assistant',
-  },
-  {
-    id: 789,
-    content: 'a sollicitudin orci sem eget massa. Suspendisse eleifend. Cras sed',
-    sender: 'user',
-  },
-  {
-    id: 790,
-    content: 'Duis sit amet diam eu dolor egestas rhoncus. Proin nisl',
-    sender: 'assistant',
-  },
-  {
-    id: 791,
-    content: 'gravida molestie arcu. Sed eu nibh vulputate mauris sagittis placerat.',
-    sender: 'user',
-  },
-  {
-    id: 792,
-    content: 'Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie',
-    sender: 'assistant',
-  },
-  {
-    id: 793,
-    content: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
-    sender: 'user',
-  },
-  {
-    id: 795,
-    content: 'metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus',
-    sender: 'assistant',
-  },
-  {
-    id: 796,
-    content: 'amet risus. Donec egestas. Aliquam nec enim. Nunc ut erat.',
-    sender: 'user',
-  },
-  {
-    id: 797,
-    content: 'Suspendisse sed dolor. Fusce mi lorem, vehicula et, rutrum eu,',
-    sender: 'assistant',
-  },
-  {
-    id: 798,
-    content: 'aliquet vel, vulputate eu, odio. Phasellus at augue id ante',
-    sender: 'user',
-  },
-  {
-    id: 799,
-    content: 'sollicitudin orci sem eget massa. Suspendisse eleifend. Cras sed leo.',
-    sender: 'user',
-  },
-  {
-    id: 800,
-    content: 'erat semper rutrum. Fusce dolor quam, elementum at, egestas a,',
-    sender: 'assistant',
-  },
-  {
-    id: 801,
-    content: 'nulla ante, iaculis nec, eleifend non, dapibus rutrum, justo. Praesent',
-    sender: 'user',
-  },
-  {
-    id: 802,
-    content: 'Donec sollicitudin adipiscing ligula. Aenean gravida nunc sed pede. Cum',
-    sender: 'assistant',
-  },
-  {
-    id: 803,
-    content: 'Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut',
-    sender: 'user',
-  },
-  {
-    id: 804,
-    content: 'at pretium aliquet, metus urna convallis erat, eget tincidunt dui',
-    sender: 'user',
-  },
-  {
-    id: 805,
-    content: 'Aliquam tincidunt, nunc ac mattis ornare, lectus ante dictum mi,',
-    sender: 'assistant',
-  },
-  {
-    id: 806,
-    content: 'ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis',
-    sender: 'user',
-  },
-  {
-    id: 807,
-    content: 'elit. Aliquam auctor, velit eget laoreet posuere, enim nisl elementum',
-    sender: 'assistant',
-  },
-  {
-    id: 808,
-    content: 'ornare, lectus ante dictum mi, ac mattis velit justo nec',
-    sender: 'user',
-  },
-];
+// const mockData = [
+//   {
+//     id: 123,
+//     content: '안녕',
+//     sender: 'user',
+//   },
+//   {
+//     id: 456,
+//     content: '무슨 얘기를 할까요?',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 234,
+//     content: '안녕',
+//     sender: 'user',
+//   },
+//   {
+//     id: 1233,
+//     content: '무슨 얘기를 할까요?',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 789,
+//     content: 'a sollicitudin orci sem eget massa. Suspendisse eleifend. Cras sed',
+//     sender: 'user',
+//   },
+//   {
+//     id: 790,
+//     content: 'Duis sit amet diam eu dolor egestas rhoncus. Proin nisl',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 791,
+//     content: 'gravida molestie arcu. Sed eu nibh vulputate mauris sagittis placerat.',
+//     sender: 'user',
+//   },
+//   {
+//     id: 792,
+//     content: 'Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 793,
+//     content: 'amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt',
+//     sender: 'user',
+//   },
+//   {
+//     id: 795,
+//     content: 'metus. In lorem. Donec elementum, lorem ut aliquam iaculis, lacus',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 796,
+//     content: 'amet risus. Donec egestas. Aliquam nec enim. Nunc ut erat.',
+//     sender: 'user',
+//   },
+//   {
+//     id: 797,
+//     content: 'Suspendisse sed dolor. Fusce mi lorem, vehicula et, rutrum eu,',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 798,
+//     content: 'aliquet vel, vulputate eu, odio. Phasellus at augue id ante',
+//     sender: 'user',
+//   },
+//   {
+//     id: 799,
+//     content: 'sollicitudin orci sem eget massa. Suspendisse eleifend. Cras sed leo.',
+//     sender: 'user',
+//   },
+//   {
+//     id: 800,
+//     content: 'erat semper rutrum. Fusce dolor quam, elementum at, egestas a,',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 801,
+//     content: 'nulla ante, iaculis nec, eleifend non, dapibus rutrum, justo. Praesent',
+//     sender: 'user',
+//   },
+//   {
+//     id: 802,
+//     content: 'Donec sollicitudin adipiscing ligula. Aenean gravida nunc sed pede. Cum',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 803,
+//     content: 'Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut',
+//     sender: 'user',
+//   },
+//   {
+//     id: 804,
+//     content: 'at pretium aliquet, metus urna convallis erat, eget tincidunt dui',
+//     sender: 'user',
+//   },
+//   {
+//     id: 805,
+//     content: 'Aliquam tincidunt, nunc ac mattis ornare, lectus ante dictum mi,',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 806,
+//     content: 'ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis',
+//     sender: 'user',
+//   },
+//   {
+//     id: 807,
+//     content: 'elit. Aliquam auctor, velit eget laoreet posuere, enim nisl elementum',
+//     sender: 'assistant',
+//   },
+//   {
+//     id: 808,
+//     content: 'ornare, lectus ante dictum mi, ac mattis velit justo nec',
+//     sender: 'user',
+//   },
+// ];
 
 const BookChat = () => {
   const { classes } = useStyles();
@@ -197,9 +197,9 @@ const BookChat = () => {
   const [messageInput, setMessageInput] = React.useState('');
 
   //임시
-  React.useEffect(() => {
-    setMessages(mockData);
-  }, []);
+  // React.useEffect(() => {
+  //   setMessages(mockData);
+  // }, []);
 
   const handleSendMessage = () => {
     if (messageInput.trim() !== '') {
@@ -217,11 +217,11 @@ const BookChat = () => {
     setMessageInput(event.target.value);
   };
 
-  // const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-  //   if ((event.ctrlKey || event.metaKey) && event.key === 'Enter') {
-  //     handleSendMessage();
-  //   }
-  // };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if ((event.ctrlKey || event.metaKey) && event.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
 
   return (
     <Container className={classes.container}>
@@ -259,7 +259,7 @@ const BookChat = () => {
           minRows={2}
           maxRows={4}
           onChange={handleChange}
-          // onKeyPress={handleKeyPress}
+          onKeyDown={handleKeyDown}
           rightSection={
             <ActionIcon variant="light" onClick={handleSendMessage} size={35}>
               <IconSend size={20} />
