17ce8ffa9ff643841c5b4a736be851a960d9167d
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-01 21:43:16 +0900

    feat: 채팅 메시지 업데이트 api 구현

---
+++ src/api/chat.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { URL } from '../constants';
+
+interface Message {
+  id: number;
+  content: string;
+  sender: string;
+}
+
+const config = {
+  baseURL: 'http://localhost:3000',
+};
+
+const updateChat = async (message: Message) =>
+  axios.post(
+    `${URL.CHAT}`,
+    {
+      id: message.id,
+      content: message.content.replace(/\n/g, ' '),
+      sender: message.sender,
+    },
+    config
+  );
+
+export { updateChat };
