6d543977741a0faa3de8e469c95724d74c759612
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-04 13:20:20 +0900

    feat: ResultCard를 컴포넌트로 분리

--- src/pages/RecommendResult.tsx
+++ src/pages/RecommendResult.tsx
@@ -1,6 +1,6 @@
-import { createStyles, Text, Group, Button, rem, Title, Paper, Flex } from '@mantine/core';
+import { Text, Group, Button, Title, Flex } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
-import { IconSlash } from '@tabler/icons-react';
+import { ResultCard } from '../components/recommend';
 
 const mockData = {
   comment: '맑은 날씨에 추리 소설을 추천해드리겠습니다. 제 추천은 다음과 같습니다.',
@@ -22,62 +22,6 @@ const mockData = {
         '히가시노 게이고의 작품은 일본 추리 소설의 대표적인 작품 중 하나입니다. 꼬리물기 식으로 매우 흥미진진하며 독자를 계속해서 긴장시킵니다.',
     },
   ],
-};
-
-const useStyles = createStyles(theme => ({
-  resultCard: {
-    height: rem(440),
-    backgroundImage: theme.fn.gradient(),
-  },
-
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontWeight: 900,
-    color: theme.white,
-    lineHeight: 1.2,
-    fontSize: rem(32),
-  },
-
-  author: {
-    color: theme.white,
-    opacity: 0.7,
-    fontWeight: 700,
-    textTransform: 'uppercase',
-  },
-
-  slash: {
-    color: 'white',
-    strokeWidth: rem(1),
-  },
-}));
-
-interface CardProps {
-  id: number;
-  title: string;
-  author: string;
-  reason: string;
-}
-
-const ResultCard = ({ title, author, reason }: CardProps) => {
-  const { classes } = useStyles();
-
-  return (
-    <Paper shadow="md" p={50} radius="md" className={classes.resultCard}>
-      <Flex direction={'column'} align={'center'} mx={20} justify={'space-between'}>
-        <Text className={classes.author} size="xs" mb={'sm'}>
-          {author}
-        </Text>
-        <IconSlash className={classes.slash} />
-        <Title order={3} my={'sm'} className={classes.title}>
-          {title}
-        </Title>
-        <IconSlash className={classes.slash} />
-        <Text color="white" mt={'sm'}>
-          {reason}
-        </Text>
-      </Flex>
-    </Paper>
-  );
 };
 
 const RecommendResult = () => {
