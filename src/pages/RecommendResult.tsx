b712fd4b2fc2f929ceaefa4ccbcfbba7569460ec
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-04 18:44:58 +0900

    feat: 전달받은 결과 데이터로 렌더링되도록 구현

--- src/pages/RecommendResult.tsx
+++ src/pages/RecommendResult.tsx
@@ -1,41 +1,23 @@
 import { Text, Group, Button, Title, Flex } from '@mantine/core';
 import { Carousel } from '@mantine/carousel';
 import { ResultCard } from '../components/recommend';
-import { Link } from 'react-router-dom';
-
-const mockData = {
-  comment: '맑은 날씨에 추리 소설을 추천해드리겠습니다. 제 추천은 다음과 같습니다.',
-
-  books: [
-    {
-      id: 1,
-      title: '셜록 홈즈',
-      author: '아서 코난 도일',
-      reason:
-        "클래식한 추리 소설로 유명한 '셜록 홈즈' 시리즈는 명품입니다. 매우 유명한 캐릭터를 다루고 있어 읽는 동안 흥미로움을 느낄 수 있습니다.",
-    },
-
-    {
-      id: 2,
-      title: '용의자 X의 헌신',
-      author: '히가시노 게이고',
-      reason:
-        '히가시노 게이고의 작품은 일본 추리 소설의 대표적인 작품 중 하나입니다. 꼬리물기 식으로 매우 흥미진진하며 독자를 계속해서 긴장시킵니다.',
-    },
-  ],
-};
+import { Link, useLocation } from 'react-router-dom';
 
 const RecommendResult = () => {
-  const slides = mockData.books.map(book => (
-    <Carousel.Slide key={book.title}>
-      <ResultCard {...book} />
+  const location = useLocation();
+  const resultData = location.state;
+
+  const slides = resultData.books.map(data => (
+    <Carousel.Slide key={data.title}>
+      <ResultCard {...data} />
     </Carousel.Slide>
   ));
 
   return (
     <Flex direction={'column'} align={'center'} gap={20} mt={20}>
       <Title>Book Chat이 추천합니다!</Title>
-      <Text>{mockData.comment}</Text>
+      <Text>{resultData.firstComment}</Text>
+      <Text>{resultData.lastComment}</Text>
       <Carousel miw={300} maw={500} mx={'auto'} slideGap="xl" align="center" withIndicators loop>
         {slides}
       </Carousel>
