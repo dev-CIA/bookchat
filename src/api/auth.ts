5a4871342dc3f799fcfa9c1aed82e4d7084c4006
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-05 16:39:03 +0900

    feat: signin signup type 설정

---
+++ src/api/auth.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import { URL } from '../constants';
+import { z } from 'zod';
+import { signinSchema, signupSchema } from '../schema';
+
+type signinForm = z.infer<typeof signinSchema>;
+type signupForm = z.infer<typeof signupSchema>;
