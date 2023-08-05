309de9b25f5e4f2db4e4eb268b859b238d83583e
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-06 01:57:29 +0900

    fix: subTitle 이름이 path name따라 바뀌도록 수정

--- src/components/root/MobileHeader.tsx
+++ src/components/root/MobileHeader.tsx
@@ -1,6 +1,7 @@
 import { Header, Title, createStyles } from '@mantine/core';
-import { sizes } from '../../constants';
+import { sizes, links } from '../../constants';
 import { UserMenu } from '.';
+import { useLocation } from 'react-router-dom';
 
 const useStyles = createStyles(() => ({
   header: {
@@ -13,10 +14,13 @@ const useStyles = createStyles(() => ({
 
 const MobileHeader = () => {
   const { classes } = useStyles();
+  const { pathname } = useLocation();
 
   return (
     <Header className={classes.header} height={sizes.MOBILE_HEADER_HEIGHT} px={16}>
-      <Title size={20}>subTitle</Title>
+      <Title size={20} fw={600}>
+        {pathname === '/' ? 'Book Chat' : links.mainLinks.filter(link => link.link === pathname)[0].label}
+      </Title>
       <UserMenu />
     </Header>
   );
