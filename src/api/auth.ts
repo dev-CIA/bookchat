7f3edb54ccfc739327e4bae7bfacdc1c3e7f0665
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-05 17:02:37 +0900

    feat: 로그인 회원가입 api 작성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -9,3 +9,9 @@ type signupForm = z.infer<typeof signupSchema>;
 const config = {
   baseURL: 'http://localhost:3000/api/auth',
 };
+
+const signin = async (data: signinForm) => await axios.post(`${URL.SIGN_IN}`, { ...data }, config);
+
+const singup = async (data: signupForm) => await axios.post(`${URL.SIGN_UP}`, { ...data }, config);
+
+export { signin, singup };
