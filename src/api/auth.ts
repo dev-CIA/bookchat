2cf65dd82122f45409fe727b54e005db88be1660
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-05 16:39:24 +0900

    feat: config 설정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -5,3 +5,7 @@ import { signinSchema, signupSchema } from '../schema';
 
 type signinForm = z.infer<typeof signinSchema>;
 type signupForm = z.infer<typeof signupSchema>;
+
+const config = {
+  baseURL: 'http://localhost:3000/api/auth',
+};
