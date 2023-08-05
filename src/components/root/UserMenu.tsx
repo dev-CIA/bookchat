021566dc809183ad51347d1ba5fbe6d183b42efd
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-05 18:12:31 +0900

    feat: 로그아웃 구현

--- src/components/root/UserMenu.tsx
+++ src/components/root/UserMenu.tsx
@@ -1,7 +1,20 @@
 import { Menu, UnstyledButton, Avatar } from '@mantine/core';
 import { IconSettings, IconLogout } from '@tabler/icons-react';
+import { useSetRecoilState } from 'recoil';
+import { isLoginState } from '../../recoil/atoms';
+import { singout } from '../../api';
 
 const UserMenu = () => {
+  const setIsLogin = useSetRecoilState(isLoginState);
+
+  const handleClick = async () => {
+    const { data } = await singout();
+
+    localStorage.removeItem('userState');
+    localStorage.removeItem('menuState');
+    setIsLogin(data.isLogin);
+  };
+
   return (
     <Menu
       shadow="md"
@@ -23,7 +36,7 @@ const UserMenu = () => {
         <Menu.Divider />
 
         <Menu.Label></Menu.Label>
-        <Menu.Item color="red" icon={<IconLogout size={14} />}>
+        <Menu.Item color="red" icon={<IconLogout size={14} />} onClick={handleClick}>
           로그아웃
         </Menu.Item>
       </Menu.Dropdown>
