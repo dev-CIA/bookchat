3e198a4cf9b6850edd2e38a45d2588f52f4e69a2
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-11 00:05:12 +0900

    feat: 책 검색 옵션 선택하는 UI 구현

--- src/pages/MyLibrary.tsx
+++ src/pages/MyLibrary.tsx
@@ -1,12 +1,33 @@
 import { Books } from '../components/myLibrary';
-import { ActionIcon, TextInput, createStyles } from '@mantine/core';
-import { IconBook, IconSearch } from '@tabler/icons-react';
+import { ActionIcon, TextInput, createStyles, Select, rem } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
 
 const useStyles = createStyles(() => ({
   form: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  selectInput: {
+    borderRight: 'transparent',
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    fontSize: rem(8),
+    ':focus': {
+      borderColor: '#ced4da',
+    },
+  },
+  selectItem: {
+    padding: rem(8),
+    fontSize: rem(8),
+  },
+  searchInput: {
+    borderLeft: 'transparent',
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    ':focus': {
+      borderColor: '#ced4da',
+    },
   },
 }));
 
@@ -16,11 +37,19 @@ const MyLibrary = () => {
   return (
     <>
       <form className={classes.form}>
+        <Select
+          classNames={{ input: classes.selectInput, item: classes.selectItem }}
+          w={'20%'}
+          miw={100}
+          data={['통합 검색', '내 서재에서']}
+          defaultValue={'통합 검색'}
+          aria-label="search type select"
+        />
         <TextInput
-          placeholder="도서 검색"
-          icon={<IconBook />}
+          classNames={{ input: classes.searchInput }}
+          placeholder="책 검색하기"
           w={'60%'}
-          miw={300}
+          miw={250}
           rightSection={
             <ActionIcon variant="filled" size={'md'} title="search">
               <IconSearch size="1rem" />
