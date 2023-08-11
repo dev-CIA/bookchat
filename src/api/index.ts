397a7c32d612d848cb1c0a322323cf6a204ac120
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-11 23:17:46 +0900

    feat: 북챗 서버로 aladin api 요청 사항 우회 요청하는 api추가
    - aladin api의 cors 비허용으로 우회 요청을 위함
    - query string을 통해 aladin api 요청 파라미터를 보냄
    
    #19

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export { updateChat } from './openai';
 export { signin, singup, singout } from './auth';
 export { getMyLibrary } from './user';
+export { getAladinSearchResults } from './aladinSearch';
