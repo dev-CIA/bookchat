d3383bc43b3e90d387d83d4302f53b8a01c1e6cd
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-16 14:51:32 +0900

    feat: suspense 옵션 기본값 true로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ import { Notifications } from '@mantine/notifications';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const App = () => {
   return (
