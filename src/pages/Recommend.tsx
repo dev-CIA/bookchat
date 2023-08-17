a7aa1c930d06260613b61358301c8803168c23fc
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-17 19:53:06 +0900

    feat: suspense 사용

--- src/pages/Recommend.tsx
+++ src/pages/Recommend.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const Recommend = () => {
   console.log();
   return (
-    <>
+    <React.Suspense fallback={<div>...loading</div>}>
       <Outlet />
-    </>
+    </React.Suspense>
   );
 };
 
