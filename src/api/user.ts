f8d3bf75219376a996da37ba1e44e7aaa38cdb0c
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-19 23:43:04 +0900

    refactor: getMyLibrary api 호출시 data만 반환하도록 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,6 +4,9 @@ const config = {
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/users`,
 };
 
-const getMyLibrary = async (email: string) => await axios.get(`/${email}/library`, config);
+const getMyLibrary = async (email: string) => {
+  const { data } = await axios.get(`/${email}/library`, config);
+  return data;
+};
 
 export { getMyLibrary };
