243d9f81fa15f57b86ba86ebbbc8bd805ca7b558
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-19 23:51:46 +0900

    fix: 로그인 하지 않았을 경우 early return 처리하도록 수정

--- src/hooks/queries/useMyLibraryQuery.ts
+++ src/hooks/queries/useMyLibraryQuery.ts
@@ -4,6 +4,7 @@ import { userState } from '../../recoil/atoms';
 import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '../../constants';
 import { getMyLibrary } from '../../api';
+import { isLoginState } from '../../recoil/atoms';
 import type { BookApiData } from '../../types/bookData';
 
 interface optionsProps {
@@ -11,6 +12,9 @@ interface optionsProps {
 }
 
 const useMyLibraryQuery = (options?: optionsProps) => {
+  const isLogin = useRecoilValue(isLoginState);
+  if (!isLogin) return { libraryData: [''] };
+
   const { email } = useRecoilValue(userState);
 
   const query = useQuery<BookApiData[], AxiosError, BookApiData[] | string[], string[]>({
