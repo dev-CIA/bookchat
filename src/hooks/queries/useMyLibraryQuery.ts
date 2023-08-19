26a75357dcee8165fbbdb771a9cb503c00e89240
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-19 23:48:55 +0900

    refactor: select를 인수로 받도록 수정

--- src/hooks/queries/useMyLibraryQuery.ts
+++ src/hooks/queries/useMyLibraryQuery.ts
@@ -1,15 +1,22 @@
+import { AxiosError } from 'axios';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../recoil/atoms';
 import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '../../constants';
 import { getMyLibrary } from '../../api';
+import type { BookApiData } from '../../types/bookData';
 
-const useMyLibraryQuery = () => {
+interface optionsProps {
+  select?: (data: BookApiData[]) => string[];
+}
+
+const useMyLibraryQuery = (options?: optionsProps) => {
   const { email } = useRecoilValue(userState);
 
-  const query = useQuery({
+  const query = useQuery<BookApiData[], AxiosError, BookApiData[] | string[], string[]>({
     queryKey: [QUERY_KEY.MY_LIBRARY],
-    queryFn: async () => await getMyLibrary(email),
+    queryFn: () => getMyLibrary(email),
+    ...options,
   });
 
   return { ...query, libraryData: query.data?.data };
