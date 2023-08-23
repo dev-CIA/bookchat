561c9bce552e041d3b3864aacd42fc2a9d35bb88
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-23 11:16:07 +0900

    fix: 매개변수 기본값 타입 수정
    - 문자열에서 number로 수정

--- src/api/aladinSearch.ts
+++ src/api/aladinSearch.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const getAladinSearchResults = async (Query: string, QueryType = 'Keyword', Start = '1') => {
+const getAladinSearchResults = async (Query: string, QueryType = 'Keyword', Start = 1) => {
   const { data } = await axios.get(
     `${
       import.meta.env.VITE_BASE_URL
