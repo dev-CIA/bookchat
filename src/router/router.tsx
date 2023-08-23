de2ed839060233851b3ebe1651424fc7c96f4a07
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-23 11:13:02 +0900

    feat: loader에 queryClient를 파라미터로 전달

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,59 +4,61 @@ import { Recommend, RecommendResult, RecommendForm } from '../pages/recommend';
 import { MyLibrary, Books, SearchResults } from '../pages/myLibrary';
 import AuthenticationGuard from '../guard/AuthenticationGuard';
 import { searchLoader } from './loaders';
+import { QueryClient } from '@tanstack/react-query';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: 'chat',
-        element: <BookChat />,
-      },
-      {
-        path: 'recommend',
-        element: <Recommend />,
-        children: [
-          {
-            path: '',
-            element: <RecommendForm />,
-          },
-          {
-            path: 'result',
-            element: <RecommendResult />,
-          },
-        ],
-      },
-      {
-        path: 'mylibrary',
-        element: <AuthenticationGuard redirectTo="/signin" element={<MyLibrary />} />,
-        children: [
-          {
-            path: '',
-            element: <Books />,
-          },
-          {
-            path: 'search',
-            element: <SearchResults />,
-            loader: searchLoader,
-          },
-        ],
-      },
-      {
-        path: 'profile',
-        element: <Profile />,
-      },
-    ],
-  },
-  {
-    path: '/signin',
-    element: <Signin />,
-  },
-  {
-    path: '/signup',
-    element: <Signup />,
-  },
-]);
+const router = (queryClient: QueryClient) =>
+  createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [
+        {
+          path: 'chat',
+          element: <BookChat />,
+        },
+        {
+          path: 'recommend',
+          element: <Recommend />,
+          children: [
+            {
+              path: '',
+              element: <RecommendForm />,
+            },
+            {
+              path: 'result',
+              element: <RecommendResult />,
+            },
+          ],
+        },
+        {
+          path: 'mylibrary',
+          element: <AuthenticationGuard redirectTo="/signin" element={<MyLibrary />} />,
+          children: [
+            {
+              path: '',
+              element: <Books />,
+            },
+            {
+              path: 'search',
+              element: <SearchResults />,
+              loader: searchLoader(queryClient),
+            },
+          ],
+        },
+        {
+          path: 'profile',
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: '/signin',
+      element: <Signin />,
+    },
+    {
+      path: '/signup',
+      element: <Signup />,
+    },
+  ]);
 
 export default router;
