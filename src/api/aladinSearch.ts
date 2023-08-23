dfc9f6e4f8ecc7239d9c3210c7e0e436496ae665
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-23 11:17:34 +0900

    refactor: 인수 이름 수정
    - camel case로 변경
    - start -> startPage 로 변경

--- src/api/aladinSearch.ts
+++ src/api/aladinSearch.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const getAladinSearchResults = async (Query: string, QueryType = 'Keyword', Start = 1) => {
+const getAladinSearchResults = async (query: string, queryType = 'Keyword', startPage = 1) => {
   const { data } = await axios.get(
     `${
       import.meta.env.VITE_BASE_URL
-    }/api/aladin/search?Query=${Query}&QueryType=${QueryType}&Start=${Start}&Cover=Big&Output=JS&Version=20131101`
+    }/api/aladin/search?Query=${query}&QueryType=${queryType}&Start=${startPage}&Cover=Big&Output=JS&Version=20131101`
   );
   return data;
 };
