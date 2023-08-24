1bd62c417b43227f38687ad83600d675202140c5
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-24 14:55:56 +0900

    fix: searchLoader에서 캐시데이터 없을때만 서버에서 fetch하도록 수정
    - queryClient를 파라미터로 전달받아서 사용하도록 수정
    - getQueryData로 캐시 데이터 확인하여 가져오도록 함
    - 캐시데이터가 없어서 undefined일 경우 fetchInfiniteQuery를 수행하여 infinite query를 fetch하고 캐시하도록 함
    - 반환값에 book을 추가하여 검색어도 함께 반환하도록 함
    
    #20

--- src/router/loaders/searchLoader.ts
+++ src/router/loaders/searchLoader.ts
@@ -1,15 +1,24 @@
-import { getAladinSearchResults } from '../../api';
+import { InfiniteData, QueryClient } from '@tanstack/react-query';
+import { searchResultQuery } from '../../utils';
+import { SearchResponse } from '../../types';
 
-const searchLoader = async ({ request }: { request: Request }) => {
-  const url = new URL(request.url);
-  const type = url.searchParams.get('type') || '';
-  const book = url.searchParams.get('book') || '';
-  let response;
+const searchLoader =
+  (queryClient: QueryClient) =>
+  async ({ request }: { request: Request }) => {
+    const url = new URL(request.url);
+    const type = url.searchParams.get('type') || '';
+    const book = url.searchParams.get('book') || '';
 
-  if (type === '통합 검색') {
-    response = await getAladinSearchResults(book);
-  }
-  return response;
-};
+    const query = searchResultQuery(book);
+
+    if (type === '통합 검색') {
+      return {
+        initialData:
+          queryClient.getQueryData<InfiniteData<SearchResponse>>(query.queryKey) ??
+          (await queryClient.fetchInfiniteQuery(query)),
+        book,
+      };
+    }
+  };
 
 export default searchLoader;
