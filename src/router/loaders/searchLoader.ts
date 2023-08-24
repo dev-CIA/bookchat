1b44315b287c08aff659e8f9453f3859b6cb3d8f
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-24 15:16:47 +0900

    refactor: 검색 타입 분기처리 전에 return값의 type일치를 위해 임시 주석 처리

--- src/router/loaders/searchLoader.ts
+++ src/router/loaders/searchLoader.ts
@@ -6,19 +6,19 @@ const searchLoader =
   (queryClient: QueryClient) =>
   async ({ request }: { request: Request }) => {
     const url = new URL(request.url);
-    const type = url.searchParams.get('type') || '';
+    // const type = url.searchParams.get('type') || '';
     const book = url.searchParams.get('book') || '';
 
     const query = searchResultQuery(book);
 
-    if (type === '통합 검색') {
-      return {
-        initialData:
-          queryClient.getQueryData<InfiniteData<SearchResponse>>(query.queryKey) ??
-          (await queryClient.fetchInfiniteQuery(query)),
-        book,
-      };
-    }
+    // if (type === '통합 검색') {
+    return {
+      initialData:
+        queryClient.getQueryData<InfiniteData<SearchResponse>>(query.queryKey) ??
+        (await queryClient.fetchInfiniteQuery(query)),
+      book,
+    };
+    // }
   };
 
 export default searchLoader;
