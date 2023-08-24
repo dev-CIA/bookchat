a205494d0eab39717ddd4bf178899ed97932f236
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-24 15:14:25 +0900

    feat: 무한 스크롤 구현
    - intersectionObserverAPI 사용
    
    #20

--- src/pages/myLibrary/SearchResults.tsx
+++ src/pages/myLibrary/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { Container, Stack } from '@mantine/core';
+import React from 'react';
+import { Container, Flex, Loader, Stack } from '@mantine/core';
 import { useLoaderData } from 'react-router-dom';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { SearchResult } from '../../components/myLibrary';
@@ -14,11 +15,34 @@ const SearchResults = () => {
     getNextPageParam: lastPage => lastPage.startIndex + 1 ?? undefined,
   });
 
+  const observerTargetRef = React.useRef<HTMLDivElement | null>(null);
+
+  const handleObserver = React.useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage]
+  );
+
+  React.useEffect(() => {
+    const observerTarget = observerTargetRef.current;
+    const option = { threshold: 0 };
+
+    const observer = new IntersectionObserver(handleObserver, option);
+    observer.observe(observerTarget as Element);
+    return () => observer.unobserve(observerTarget as Element);
+  }, [observerTargetRef, handleObserver]);
+
   return (
     <Container mt={20}>
       <Stack>
         {data?.pages.map(page => page.item.map((book: BookApiData) => <SearchResult key={book.itemId} {...book} />))}
       </Stack>
+      <Flex ref={observerTargetRef} justify={'center'} mt={20}>
+        {isFetchingNextPage && hasNextPage && <Loader variant="dots" />}
+      </Flex>
     </Container>
   );
 };
