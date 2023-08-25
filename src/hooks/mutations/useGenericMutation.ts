7de749198cac6ae59de476bfde78d2caf2a528c0
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-25 15:50:25 +0900

    feat: useGenericMutation이 useMutation을 반환하도록 함
    - 낙관적 업데이트를 하도록 onMutate를 구현
    
    #24

--- src/hooks/mutations/useGenericMutation.ts
+++ src/hooks/mutations/useGenericMutation.ts
@@ -1,7 +1,20 @@
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
 
-const useGenericMutation = () => {
+const useGenericMutation = ({ queryKey, mutationFn, updater }) => {
   const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn,
+
+    async onMutate(variables: type) {
+      await queryClient.cancelQueries({ queryKey });
+
+      const previousDatas = queryClient.getQueryData(queryKey);
+      queryClient.setQueriesData(queryKey, updater);
+
+      return { previousDatas };
+    },
+  });
 };
 
 export default useGenericMutation;
