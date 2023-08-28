5f6d3fbf12fbb1f24dbd436900ba3b96e383785d
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-28 19:57:25 +0900

    feat: 내 서재 데이터를 ResearchResult 컴포넌트 props로 전달
    - 내 서재에 존재 유무에 따른 버튼 비활성화를 위하여 데이터 전달

--- src/pages/myLibrary/SearchResults.tsx
+++ src/pages/myLibrary/SearchResults.tsx
@@ -4,6 +4,7 @@ import { InfiniteData } from '@tanstack/react-query';
 import { SearchResult } from '../../components/myLibrary';
 import { useInfiniteScroll } from '../../hooks';
 import type { BookApiData, SearchResponse } from '../../types';
+import { useMyLibraryQuery } from '../../hooks/queries';
 
 interface SearchLoaderData {
   initialData: InfiniteData<SearchResponse>;
@@ -13,11 +14,18 @@ interface SearchLoaderData {
 const SearchResults = () => {
   const { searchWord } = useLoaderData() as SearchLoaderData;
   const { data, isFetchingNextPage, hasNextPage, observerTargetRef } = useInfiniteScroll(searchWord);
+  const { libraryData: libraryIds } = useMyLibraryQuery({
+    select: libraryData => libraryData.map(item => item.itemId),
+  });
 
   return (
     <Container mt={20}>
       <Stack>
-        {data?.pages.map(page => page.item.map((book: BookApiData) => <SearchResult key={book.itemId} {...book} />))}
+        {data?.pages.map(page =>
+          page.item.map((book: BookApiData) => (
+            <SearchResult key={book.itemId} book={book} libraryIds={libraryIds as number[]} />
+          ))
+        )}
       </Stack>
       <Flex ref={observerTargetRef} justify={'center'} mt={20}>
         {isFetchingNextPage && hasNextPage && <Loader variant="dots" />}
