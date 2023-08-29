66452e448f0037e66b4b0b97ea7ca94cd7af1ba5
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-29 15:38:58 +0900

    fix: 타입 수정

--- src/hooks/mutations/useGenericMutation.ts
+++ src/hooks/mutations/useGenericMutation.ts
@@ -1,16 +1,26 @@
 import { useQueryClient, useMutation } from '@tanstack/react-query';
 
-const useGenericMutation = ({ queryKey, mutationFn, updater }) => {
+interface GenericMutationProps<TData, TVariables, TContext> {
+  queryKey: string[];
+  mutationFn: (variables: TVariables) => Promise<TData>;
+  onMutate: (variables: TVariables) => Promise<TContext | undefined> | TContext | undefined;
+}
+
+const useGenericMutation = <TData, TVariables, TContext>({
+  queryKey,
+  mutationFn,
+  onMutate: updater,
+}: GenericMutationProps<TData, TVariables, TContext>) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn,
 
-    async onMutate(variables: type) {
+    async onMutate(variables) {
       await queryClient.cancelQueries({ queryKey });
 
       const previousDatas = queryClient.getQueryData(queryKey);
-      queryClient.setQueryData(queryKey, updater);
+      queryClient.setQueryData(queryKey, updater(variables));
 
       return { previousDatas };
     },
