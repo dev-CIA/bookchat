24799c4ab98e28a533f4579718dffade7a787206
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-30 15:20:08 +0900

    feat: 책 상세 페이지 UI 구현
    #23

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,3 +5,5 @@ export { default as BookChat } from './BookChat';
 export { default as Signin } from './Signin';
 export { default as Signup } from './Signup';
 export { default as Profile } from './Profile';
+
+export { default as BookDetail } from './BookDetail';
