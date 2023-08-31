3f80f8ed84d39b39939563cd8eff4012310f7f95
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-31 10:30:18 +0900

    refactor: util 함수명 변경
    - queryKey와 qeuryFn을 반환하는 함수임을 보다 명시적으로 나타내기 위함

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export { default as searchResultQuery } from './searchResultQuery';
-export { default as myLibraryQuery } from './myLibraryQuery';
+export { default as getSearchResultQuery } from './getSearchResultQuery';
+export { default as getMyLibraryQuery } from './getMyLibraryQuery';
 export { default as unescape } from './unescape';
